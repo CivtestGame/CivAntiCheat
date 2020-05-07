07aa5b4614d1a5c72332e7deeeef164ccc7d1e7c
R3R3R3 <>
2020-05-08 00:17:42 +0200

    Improve API + basic flyhack detection

--- api.lua
+++ api.lua
@@ -13,15 +13,21 @@ end
 
 function ac.register_module(def)
    def.name = def.name or error("Anticheat module registered without a name.")
-   def.severity = def.severity or 1
+
+   def.severity = def.severity or 1.1
+   def.threshold = def.threshhold or 2
+   def.decay = def.decay or 1.01
+
    def.violations = {}
+   def.violation_level = {}
+
    def.enabled = def.enabled or true
 
    def.log = ac.log
 
    -- Execute the check every half-second
    def.check = def.check or nil
-   def.frequency = def.frequency or 0.5
+   def.check_frequency = def.check_frequency or 0.5
    -- Module-local timer is used by the globalstep
    def._timer = 0
 
@@ -29,22 +35,21 @@ function ac.register_module(def)
    --                        and math.min(ac._lowest_timer, def._timer))
    --    or def._timer
 
-   if def.frequency and not def.check then
+   if def.check_frequency and not def.check then
       error("Anticheat module '" .. def.name
-               .. "' registered with a frequency but no check.")
+               .. "' registered with a check_frequency but no check.")
       return
    end
 
    ac.modules[def.name] = def
 end
 
-function ac.record_violation(acmodule_name, player_name)
-   local acmodule = ac.modules[acmodule_name]
+function ac.record_violation(acmodule, player_name)
 
    if not acmodule then
       minetest.log(
          "warning", "[CivAntiCheat] Violation recorded for unknown module '"
-            .. acmodule_name .. "'."
+            .. ((acmodule and acmodule.name) or "UNKNOWN") .. "'."
       )
       return
    end
@@ -58,8 +63,35 @@ function ac.record_violation(acmodule_name, player_name)
 
    acmodule.violation_level[player_name]
       = acmodule.violation_level[player_name] or 1
+
+   local vl_tab = acmodule.violation_level
+
+   local threshold = acmodule.threshold
+   local severity = acmodule.severity
+
+   local vl = vl_tab[player_name]
+
+   local new_vl = vl * severity
+
+   if new_vl > threshold then
+      local pinfo = minetest.get_player_information(player_name)
+      acmodule:log(
+         "%s VL=%.2f exceeded threshold of %.2f (RTT %.2f Jitter %.2f)",
+         player_name, new_vl, threshold, pinfo.avg_rtt, pinfo.avg_jitter
+      )
+   end
+
+   vl_tab[player_name] = new_vl
+
 end
 
+function ac.apply_decay(acmodule, pname)
+   local vl_tab = acmodule.violation_level
+   vl_tab[pname] = vl_tab[pname] or 1
+   vl_tab[pname] = math.max(
+      vl_tab[pname] * acmodule.decay, 1.0
+   )
+end
 
 minetest.register_on_mods_loaded(function()
 
@@ -68,7 +100,7 @@ minetest.register_on_mods_loaded(function()
             for _,acmodule in pairs(ac.modules) do
                if acmodule.enabled and acmodule.check then
                   acmodule._timer = acmodule._timer + dtime
-                  if acmodule._timer >= acmodule.frequency then
+                  if acmodule._timer >= acmodule.check_frequency then
                      modules_to_check[#modules_to_check + 1] = acmodule
                      acmodule._timer = 0
                   end
@@ -80,8 +112,14 @@ minetest.register_on_mods_loaded(function()
             end
 
             for _,player in ipairs(minetest.get_connected_players()) do
+               local pname = player:get_player_name()
                for _,acmodule in ipairs(modules_to_check) do
-                  acmodule:check(player)
+                  local result = acmodule:check(player)
+                  if result then
+                     ac.record_violation(acmodule, pname)
+                  else
+                     ac.apply_decay(acmodule, pname)
+                  end
                end
             end
       end)
--- fly.lua
+++ fly.lua
@@ -1,12 +1,41 @@
 
 local ac = civanticheat
 
+local player_pos_history = {}
+
 ac.register_module({
       name = "Fly",
-      severity = 1,
+      severity = 1.2,
+      threshold = 3,
+      decay = 0.95,
 
-      frequency = 1,
+      check_frequency = 0.25,
       check = function(self, player)
-         -- self:log("It's a party with " .. player:get_player_name())
+         local pname = player:get_player_name()
+         local ppos = player:get_pos()
+
+         local ground_node = minetest.get_node(
+            vector.new(ppos.x, ppos.y - 1.5, ppos.z)
+         )
+
+         if ground_node.name ~= "air" then
+            player_pos_history[pname] = {}
+            return false
+         end
+
+         player_pos_history[pname] = player_pos_history[pname] or {}
+         table.insert(player_pos_history[pname], ppos)
+
+         local last_y
+
+         for i,hpos in ipairs(player_pos_history[pname]) do
+            if last_y and hpos.y >= last_y then
+               player_pos_history[pname] = {}
+               return true
+            end
+            last_y = hpos.y
+         end
+
+         return false
       end,
 })
