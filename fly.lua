b746efabdb13b83e3b8348bdb3ad3542134c1ec4
R3R3R3 <>
2020-07-23 22:47:50 +0200

    Fly module node-perching improvements

--- fly.lua
+++ fly.lua
@@ -22,9 +22,40 @@ ac.register_module({
          local pname = player:get_player_name()
          local ppos = player:get_pos()
 
-         local ground_node = minetest.get_node(
-            vector.new(ppos.x, ppos.y - 1.5, ppos.z)
-         )
+         local gpos = vector.new(ppos.x, ppos.y - 1, ppos.z)
+         local ground_node = minetest.get_node(gpos)
+
+         -- When the AC doesn't think we're standing on air, great, we're very
+         -- obviously grounded on a solid. No cheats here.
+         --
+         -- However, players can "perch" on the edges of nodes (e.g. sneaking),
+         -- and this, naively implemented, is interpreted as standing on air.
+         --
+         -- To account for this, we do a threshold-check of our X and Z, and
+         -- figure out a candidate for the perch node. If no perch is found, the
+         -- player is in violation.
+
+         if ground_node.name == "air" then
+            local z_dec = ac._decimal(ppos.z)
+            local ngz = ppos.z
+            if z_dec > 0.5 and z_dec < 0.8 then
+               ngz = math.floor(ppos.z)
+            elseif z_dec > 0.2 and z_dec <= 0.5 then
+               ngz = math.ceil(ppos.z)
+            end
+
+            local x_dec = ac._decimal(ppos.x)
+            local ngx = ppos.x
+            if x_dec > 0.5 and x_dec < 0.8 then
+               ngx = math.floor(ppos.x)
+            elseif x_dec > 0.2 and x_dec <= 0.5 then
+               ngx = math.ceil(ppos.x)
+            end
+
+            local ngpos = vector.new(ngx, ppos.y - 1, ngz)
+
+            ground_node = minetest.get_node(ngpos)
+         end
 
          if ground_node.name ~= "air" then
             player_pos_history[pname] = {}
--- init.lua
+++ init.lua
@@ -3,6 +3,19 @@ civanticheat = {}
 
 local path = minetest.get_modpath(minetest.get_current_modname())
 
+local ac = civanticheat
+
+function ac._round(x)
+   return x >= 0
+      and math.floor(x + 0.5)
+      or math.ceil(x - 0.5)
+end
+
+function ac._decimal(x)
+   return x - math.floor(x)
+end
+
+
 dofile(path .. "/api.lua")
 dofile(path .. "/commands.lua")
 
